590f80b0875949aae9c5422d8bed1ebba8aa54b2
Marcos Soares <mavmaso@gmail.com>
2018-06-27 18:16:21 -0300

    ultimo

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,14 @@
 class HomeController < ApplicationController 
     
-    def index
-        @cards = Card.all()  
-    end
+  def index
+    @cards = Card.all()  
+  end
 
-    def ver_traducao
-        id = params[:id]
-        @card = Card.find(id)
-    end
+  def ver_traducao
+    id = params[:id]
+    @card = Card.find(id)
+  end
 
 end
+
+
